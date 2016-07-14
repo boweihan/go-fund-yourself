a171e76e52d8e8fb3c198a369bf9c61b180a4199
Carol Yao <c.y-@hotmail.com>
2016-07-14 18:31:52 -0400

    Added purchases cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 # #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 # #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create(name: 'test', email:'test@test.com', password:'test', password_confirmation:'test')
-Project.create(name: 'test project', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit', user_id: 1, max_shares: 100)
+Project.create(name: 'test project', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit', user_id: 1, max_shares: 100, picture_url:"http://www.thebolde.com/wp-content/uploads/2015/11/iStock_000083903869_Small-400x400.jpg")
 Share.create(share_type: 'voting', price: 5, project_id:1)
 Purchase.create(number_of_shares: 100, user_id: 1, project_id: 1, share_id: 1)
